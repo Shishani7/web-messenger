53b85fcde2d35814214be6dd119e476a2e3679b4
Lepiev <st116451@student.spbu.ru>
2025-12-14 15:07:13 +0300

    feat(backend): добавлено подтверждение через временный токен и проверка токена

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,38 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs')
 const path = require('path');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // создаем приложение и задаем порт по умолчанию
 const app = express();
 const PORT = 3001;
+
+const JWT_SECRET = 'very-very-and-one-more-very-secret'; // потом надо его добавить в .env
+
 // временное хранилище пользователей (потом заменим на БД)
 const users = [];
+
+const SALT_ROUNDS = 10;
+
+// проверка токена
+const authRequired = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token required' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+    // если всё ок, пропускает запрос дальше, прикрепив данные к req.user
+    req.user = user; 
+    next();
+  });
+};
 
 // включаем поддержку JSON в теле запросов (req.body)
 app.use(express.json());
@@ -49,7 +74,9 @@ app.post('/api/echo', (req, res) => {
   })
 });
 
-app.post('/api/auth/register', (req, res) => {
+app.post('/api/auth/register', async (req, res) => {
+  
+  
   const { login, password } = req.body;
 
   if (!login || !password) {
@@ -61,10 +88,13 @@ app.post('/api/auth/register', (req, res) => {
     return res.status(400).json({ message: 'User already exists' });
   }
 
+  // хешируем пароль
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+  
   const newUser = {
     id: users.length + 1,
     login,
-    password,
+    passwordHash,
   };
 
   users.push(newUser);
@@ -75,7 +105,7 @@ app.post('/api/auth/register', (req, res) => {
   });
 });
 
-app.post('/api/auth/login', (req, res) => {
+app.post('/api/auth/login', async (req, res) => {
 
   const {login, password} = req.body;
 
@@ -90,17 +120,33 @@ app.post('/api/auth/login', (req, res) => {
   }
 
   // проверяем пароль
-  if(user.password !== password){
+  const righthash = await bcrypt.compare(password, user.passwordHash);
+  if(!righthash){
     return res.status(400).json({message: 'Wrong in login or password'});
   }
+
+  // генерируем токен
+  const token = jwt.sign(
+    { id: user.id, login: user.login },
+    JWT_SECRET,
+    { expiresIn: '1h' } // время жизни токена
+  );
 
   //успешный вход
   res.json({
     id: user.id,
-    login: user.login
+    login: user.login,
+    token
   });
 
 
+});
+
+app.get('/api/auth/me', authRequired, (req, res) => {
+  res.json({
+    message: 'Authorized access',
+    user: req.user // эти данные пришли из токена
+  });
 });
 
 https.createServer(httpsOptions, app).listen(PORT, () => {
